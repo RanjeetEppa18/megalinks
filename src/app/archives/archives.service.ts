faedf9b5541af8653bbe895b9161cd7fbf64ed8f
Ajeet Eppakayala <ajeet.eppakayala@transitioncomputing.com>
2020-04-12 17:56:17 +0530

    change path in archive

--- src/app/archives/archives.service.ts
+++ src/app/archives/archives.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment'
+import { Observable, from } from 'rxjs'
 
 @Injectable()
 export class ArchiveService {
   constructor(private http: HttpClient) {}
   getArchives() {
-    return this.http.get<any[]>('http://localhost:5000/api/archives')
+    return this.http.get<any[]>(`${environment.domain}/api/archives`)
   }
 
   getArchiveComments(archiveId) {
     return this.http.get<any[]>(
-      'http://localhost:5000/api/archivecomments/' + archiveId
+      `${environment.domain}/api/archivecomments/` + archiveId
     )
   }
 }
